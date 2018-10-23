1e447f41a7128b6376c71c8648dcb7de69d611d8
Xiyu Yi <xiyu.yi@gmail.com>
2018-10-23 15:40:25 -0700

    Update xy_gray2rgb.m

--- Library/xy_gray2rgb.m
+++ Library/xy_gray2rgb.m
@@ -1,3 +1,10 @@
+% This code is distributed under MIT license, please refer to the LICENSE file in the package for details.
+%
+%   Copyright (c) 2018 Xiyu Yi
+%
+%   Author of the code: Xiyu Yi
+%   Email of the author: xiyu.yi@gmail.com
+
 function output = xy_gray2rgb(im, map)
 im(isnan(im))= eps;
 im = double(im);

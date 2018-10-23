d899d56194787b2079f37a9bec3f53f34447b30e
Xiyu Yi <xiyu.yi@gmail.com>
2018-10-23 15:41:32 -0700

    Update xy_PixelProfiles.m

--- Library/Utils/xy_PixelProfiles.m
+++ Library/Utils/xy_PixelProfiles.m
@@ -1,5 +1,11 @@
 function [xx,yy]=xy_PixelProfiles(x, y)
-% plot y against x, as pixel profiles.
+% This code is distributed under MIT license, please refer to the LICENSE file in the package for details.
+%
+%   Copyright (c) 2018 Xiyu Yi
+%
+%   Author of the code: Xiyu Yi
+%   Email of the author: xiyu.yi@gmail.com
+%   plot y against x, as pixel profiles.
 xlen = length(x);
 xx = zeros(1,xlen*3-3);
 yy = zeros(1,xlen*3-3);

c8f7b8f7f60b4594198aa47e68a627cde5c8320f
Xiyu Yi <xiyu.yi@gmail.com>
2018-10-23 15:39:33 -0700

    Update xy_DeconvSK.m

--- Library/xy_DeconvSK.m
+++ Library/xy_DeconvSK.m
@@ -1,11 +1,10 @@
-% this script demonstrate iterative deconvolution method with shrinking
-% kernal.
-% Author: Xiyu Yi @ UCLA
-% Email: xiyu.yi@gmail.com
+% This code is distributed under MIT license, please refer to the LICENSE file in the package for details.
 %
-% 
+%   Copyright (c) 2018 Xiyu Yi
 %
-%
+%   Author of the code: Xiyu Yi
+%   Email of the author: xiyu.yi@gmail.com
+
 function output = xy_DeconvSK(para)
 J0 = para.J0; 
 PSF0 = para.PSF0;
